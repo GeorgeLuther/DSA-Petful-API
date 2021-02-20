ce85a97f836aa3ecaef7d63a7213a30d9b5d1219
George Luther <george_l@thinkful.com>
2021-02-20 18:26:13 -0500

    note to set-up resetting

--- modules/people/people.service.js
+++ modules/people/people.service.js
@@ -6,7 +6,7 @@ const store = require('../../store')
 
 const people = new Queue()
 store.people.forEach(person => people.enqueue(person))
-
+//set up for resetting?
 // --------------------
 
 module.exports = {
